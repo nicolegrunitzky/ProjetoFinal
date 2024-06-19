d7a1179c8b10f7ecc3bb8bd4d015d77268b9a9ab
nicolegrunitzky <nicole-ggrunitzky@educar.rs.gov.br>
2024-06-19 20:26:30 -0300

    Primeiro commit do projeto

---
+++ maratona/src/main/java/com/example/maratona/MaratonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.maratona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaratonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaratonaApplication.class, args);
+	}
+
+}
---
+++ maratona/src/test/java/com/example/maratona/MaratonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maratona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaratonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
